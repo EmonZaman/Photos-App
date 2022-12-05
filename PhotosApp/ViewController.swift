d133968af969709f68a4ba96cb4594a4c87eb747
EmonZaman <aagontukemon@gmail.com>
2022-12-05 10:05:59 +0600

    Something has changed

--- PhotosApp/ViewController.swift
+++ PhotosApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("something has changed")
         // Do any additional setup after loading the view.
     }
 
